a8919cb82e446b2d9f8cd8177ac404a93c054793
roman <romasaab@gmail.com>
2024-04-19 13:18:57 +0300

    fix

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,18 +1,11 @@
-// import './components/countdown';
 import './components/dinamicHeight';
-// import './components/sliders';
 import './components/animateBtn';
 import './components/accordion';
 import './components/range';
-import './components/modals';
 import './components/animations';
-// import './components/global';
-// import './components/replaceEl';
 import './components/tabs';
-// import './components/stars-rating';
 import './components/burger';
-// import './components/quiz';
-// import './components/fancybox';
+
 
 
 

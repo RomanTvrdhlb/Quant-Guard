d330dc28157d2a1f9569f383fa8d95a63196a7c1
roman <romasaab@gmail.com>
2024-04-30 22:43:57 +0300

    fix bar

--- source/js/components/simpleBar.js
+++ source/js/components/simpleBar.js
@@ -1,54 +1,52 @@
-import SimpleBar from 'simplebar';
+import SimpleBar from "simplebar";
 
-const simpleBar = new SimpleBar(document.getElementById('data-simplebar'), { autoHide: false });
-const container = simpleBar.getScrollElement();
-const parent = document.getElementById('data-simplebar').parentNode;
+const bar = document.getElementById("data-simplebar");
+if (bar) {
+  const simpleBar = new SimpleBar(bar, { autoHide: false });
+  const container = simpleBar.getScrollElement();
+  const parent = document.getElementById("data-simplebar").parentNode;
+  const span = document.createElement("span");
+  span.classList.add("simplebar-line");
+  span.style.display = "block";
 
-// Создаем элемент span
-const span = document.createElement('span');
-span.classList.add('simplebar-line');
-span.style.display = 'block';
+  const scrollTrack = document.querySelector(".simplebar-track");
+  scrollTrack.appendChild(span);
+  const scrollbarVisible = document.querySelector(".simplebar-scrollbar");
 
-// Получаем элемент .simplebar-track
-const scrollTrack = document.querySelector('.simplebar-track');
-
-// Добавляем span в scrollTrack
-scrollTrack.appendChild(span);
-
-// Получаем элемент .simplebar-scrollbar
-const scrollbarVisible = document.querySelector('.simplebar-scrollbar');
-
-function updateSpanWidth() {
+  function updateSpanWidth() {
     if (scrollbarVisible) {
-        const styles = window.getComputedStyle(scrollbarVisible);
-        const transformValue = styles.getPropertyValue('transform');
-        let tx;
+      const styles = window.getComputedStyle(scrollbarVisible);
+      const transformValue = styles.getPropertyValue("transform");
+      let tx;
 
-        const match = transformValue.match(/matrix\([^,]+,\s*[^,]+,\s*[^,]+,\s*[^,]+,\s*(-?\d+)(?:px)?,\s*[^,]+\)/);
-        if (match) {
-            tx = parseInt(match[1]);
-        }
-        let displayValue = parseInt(styles.getPropertyValue('width').replace('px', ''));
-        let widthLine = displayValue + tx - 15;
-   
-        span.style.width = `${widthLine}px`;
+      const match = transformValue.match(
+        /matrix\([^,]+,\s*[^,]+,\s*[^,]+,\s*[^,]+,\s*(-?\d+)(?:px)?,\s*[^,]+\)/
+      );
+      if (match) {
+        tx = parseInt(match[1]);
+      }
+      let displayValue = parseInt(
+        styles.getPropertyValue("width").replace("px", "")
+      );
+      let widthLine = displayValue + tx - 15;
+
+      span.style.width = `${widthLine}px`;
     }
-}
+  }
 
-// Вызываем updateSpanWidth при загрузке страницы
-window.addEventListener('load', updateSpanWidth);
+  window.addEventListener("load", updateSpanWidth);
+  window.addEventListener("resize", updateSpanWidth);
 
-// Вызываем updateSpanWidth при изменении размеров окна
-window.addEventListener('resize', updateSpanWidth);
+  container.addEventListener("scroll", function () {
+    const isAtEnd =
+      container.scrollLeft + container.clientWidth >= container.scrollWidth;
 
-container.addEventListener('scroll', function() {
-    const isAtEnd = container.scrollLeft + container.clientWidth >= container.scrollWidth;
-  
     if (isAtEnd) {
-        parent.classList.add('active');
+      parent.classList.add("active");
     } else {
-        parent.classList.remove('active');
+      parent.classList.remove("active");
     }
 
     updateSpanWidth();
-});
+  });
+}
